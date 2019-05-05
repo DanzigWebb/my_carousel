414f0f38971096798799106bd0d95baa8ad70f80
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-05-06 00:23:36 +0300

    c

--- scripts.js
+++ scripts.js
@@ -1,54 +1,67 @@
-function myCarousel (){
+$(document).ready(function () {
+  let carousel = function () {
 
-var imgBox = document.getElementById('box');
-imgBox.classList.add('product--sale');
-var imgBoxList = imgBox.getElementsByTagName('img');
+    // первоначальная разметка
+    $('.carousel-content img').wrap("<div class='carousel-item'></div>");
+    $('.slider').append("<button class='carousel-prev'>Prev</button>");
+    $('.slider').append("<button class='carousel-next'>Next</button>");
 
-// создание обертки для картинок
-for (var i = 0; i < imgBoxList.length; i++) {
-  var imgBoxItem = imgBoxList[i];
-  imgBoxItem.outerHTML = '<div class="carousel__item">' + imgBoxItem.outerHTML + '</div>';
-  var imgWrap = imgBox.getElementsByTagName('div');
-}
-// присвоение ширины обертки в 1/4 предоставленного пространства
-var widthImg = 0;
-var heightImg = 0;
+    // объявление переменных
+    let carouselWrapper = $('.carousel-wrapper');
+    let carouselContent = $('.carousel-content');
+    let carouselItem = $('.carousel-item');
 
-
-for (var j = 0; j < imgWrap.length; j++) {
-  imgWrap[j].style.width = 24 + '%';
-  imgWrap[j].style.float = 'left';
-  // определение ширины картинки
-  widthImg = imgWrap[j].offsetWidth;
-  // определение высоты картинки
-  heightImg = imgWrap[j].offsetHeight;
-};
+    let prevBtn = $('.carousel-prev')
+    let nextBtn = $('.carousel-next')
+    let sliderWidth = carouselWrapper.width();
+    let carouselStep = sliderWidth / 2;
 
 
-// расположение картинок друг за другом вправо
-var imgPosition = 0;
-for (var j = 1; j < imgWrap.length; j++) {
-  imgPosition = imgPosition + 25;
-  imgWrap[j].style.left = imgPosition + '%';
-}
-// задание высоты карусели в зависимости от картинок
-imgBox.style.height = heightImg +'px';
-// определение количество прокруток, после которой карусель возвращается в начальное положение
-var carouselRepeat = -(widthImg * (imgWrap.length - 4));
+    let logCarousel = function () {
+      // определение шага слайдера
+      let sliderWidth = carouselWrapper.width();
+      let carouselStep = sliderWidth / 2;
+      carouselItem.width(carouselStep)
 
-// создание функции прокрутки элементов
-document.getElementById('btn-left').onclick = sliderLeft;
-var imgLeft = 0;
-function sliderLeft () {
-  var imgList = document.getElementById('box');
-  imgLeft = imgLeft - widthImg;
-  // перемещение карусели влево на ширину картинки и условие ее перезапуска
-  if (imgLeft < carouselRepeat) {
-    imgLeft = 0;
-  };
-  imgList.style.left = imgLeft + 'px';
-}
+      // минимальная ширина карусели
+      // определение мин ширины как сумма ширины всех дочерних элементов
+      let carouselWidth = 0;
+      $.each(carouselItem, function () {
+        carouselWidth += 1.5 * carouselItem.width();
+      })
 
-}
+      // объявление ширины
+      carouselContent.width(carouselWidth)
+      carouselContent.height(carouselItem.height())
 
-myCarousel ();
+      // логика
+      let carouselCurrent = 0;
+      nextBtn.on('click', function () {
+        carouselContent.css("right", carouselCurrent + carouselStep + "px");
+        carouselCurrent += carouselStep;
+      })
+
+      prevBtn.on('click', function () {
+        carouselContent.css("right", carouselCurrent - carouselStep + "px");
+        carouselCurrent -= carouselStep;
+
+
+      })
+      
+      $(window).resize(function () {
+        logCarousel();
+      });
+    }
+
+    logCarousel()
+    
+
+    
+
+
+
+  }
+  carousel()
+ 
+
+});
